213a4dbad40a8c8eaed410a65ca0f7fd551096de
Luis Avila <luisavila@Luiss-MBP-3.home>
2019-03-22 14:11:40 -0400

    12. Wiring Up Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import { BrowserRouter, Router } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/stream/new" exact component={StreamCreate} />
+          <Route path="/stream/edit" exact component={StreamEdit} />
+          <Route path="/stream/delete" exact component={StreamDelete} />
+          <Route path="/stream/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
