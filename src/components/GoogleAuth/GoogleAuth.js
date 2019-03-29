059544a8e4144aad988208210bce9021119cf5ca
Luis Avila <luisavila@Luiss-MBP-3.home>
2019-03-28 21:54:25 -0400

    14. Handling Auth Status Through Redux

--- src/components/GoogleAuth/GoogleAuth.js
+++ src/components/GoogleAuth/GoogleAuth.js
@@ -63,7 +63,6 @@ class GoogleAuth extends React.Component {
 const mapStateToProps = (state) => {
   return { isSignedIn: state.auth.isSignedIn };
 };
-
 export default connect(
   mapStateToProps,
   { signIn, signOut }
