a33fb008796699b113d860ffac95219d8bdcf52b
Luis Avila <luisavila@Luiss-MBP-3.home>
2019-03-30 13:16:47 -0400

    3. Connecting Redux Form

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
+import { reducer as form_Reducer } from "redux-form";
 import authReducer from "./authReducer";
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: form_Reducer
 });
